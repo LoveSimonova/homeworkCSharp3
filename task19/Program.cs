5632ae967bd733d2f7a2af1162bade6a73b203d5
«SimLove» <simonolyuba@yandex.ru>
2023-06-01 20:12:40 +0300

    add task19

---
+++ task19/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number/10000)==(number%10))
+    if(((number/1000)%10)==(number%100)/10)
+        Console.WriteLine("Число - палиндром");
+    else
+        Console.WriteLine("Число - не палиндром");
+else
+    Console.WriteLine("Число - не палиндром");
