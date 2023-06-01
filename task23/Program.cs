a4465651c8ffc07292c63c68ac330e0f9b985f08
«SimLove» <simonolyuba@yandex.ru>
2023-06-01 21:06:49 +0300

    add task23

---
+++ task23/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count=1;
+
+if(number>0)
+    while(count<=number)
+    {
+        double res = Math.Pow(count,3);
+        Console.Write(""+res+" ");
+        count++;
+    }
+else
+    Console.WriteLine("Число меньше или равно нулю");
